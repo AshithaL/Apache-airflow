de7adfc3451770343e20303b67212c2ee427d4b0
AshithaL <you@example.com>
2020-06-04 11:31:46 +0530

    Updated covid_dag file

--- covid_dag.py
+++ covid_dag.py
@@ -20,26 +20,21 @@ table_id = 'airflow'
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(0),
-    # 'end_date': datetime(2018, 12, 30),
     'depends_on_past': False,
     'email': ['ashitha.l@nineleaps.com'],
-    # 'email_on_failure': False,
-    # 'email_on_retry': False,
-    # If a task fails, retry it once after waiting
-    # at least 5 minutes
     'retries': 1,
     'retry_delay': timedelta(minutes=2),
 }
-dag = DAG(dag_id='covid',
+dag = DAG(dag_id='covid_pipeline',
           default_args=default_args,
           description="Collecting covid data",
           schedule_interval=timedelta(days=1),
           )
 
 def fetch_covid_data():
-    req = requests.get('https://api.covidindiatracker.com/state_data.json')
-    url_data = req.text
-    data = json.loads(url_data)
+    r = requests.get('https://api.covidindiatracker.com/state_data.json')
+    r_data = r.text
+    data = json.loads(r_data)
     count = 0
     covid_data = [['date', 'state', 'number_of_cases']]
     date = datetime.datetime.today().strftime('%Y-%m-%d')
@@ -52,24 +47,24 @@ def fetch_covid_data():
     return count
 
 def load_data(**kwargs):
-    dataset_ref = client.dataset(dataset_id)
-    table_ref = dataset_ref.table(table_id)
+    bq_dataset_ref = client.dataset(dataset_id)
+    bq_table_ref = dataset_ref.table(table_id)
     job_config = bigquery.LoadJobConfig()
     job_config.source_format = bigquery.SourceFormat.CSV
     job_config.skip_leading_rows = 1
     job_config.autodetect = True
 
     with open(filename, "rb") as source_file:
-        job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+        job = client.load_table_from_file(source_file, bq_table_ref, job_config=job_config)
 
     job.result()  # Waits for table load to complete.
 
     print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
     return job.output_rows
 
-def read_the_data(**kwargs):
+def read_data(**kwargs):
 
-    ti = kwargs['ti']
+    t = kwargs['t']
     v1 = ti.xcom_pull(task_ids='load_data')
     count = ti.xcom_pull(task_ids='fetch_data')
     print("percentage = {}".format((v1/count)*100))
@@ -78,6 +73,6 @@ t1 = PythonOperator(task_id="fetch_data", python_callable=fetch_covid_data, dag=
 
 t2 = PythonOperator(task_id="load_data", python_callable=load_data, dag=dag)
 
-t3 = PythonOperator(task_id="percentage", python_callable=read_the_data, provide_context=True, dag=dag)
+t3 = PythonOperator(task_id="percentage", python_callable=read_data, provide_context=True, dag=dag)
 
 t1 >> t2 >> t3
